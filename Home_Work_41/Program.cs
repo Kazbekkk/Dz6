d4992c9168e4cfe1ac279670251bcdf3712d0458
Kazbek <bender.rodrigiz2014@gmail.com>
2023-01-26 22:50:33 +0300

    Массив

---
+++ Home_Work_41/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+Console.Clear();
+
+
+int input, size, more; string inputStr, sizeStr;
+
+while (true)
+{
+    Console.WriteLine("Введите размер массива: ");
+    sizeStr = Console.ReadLine();
+    if (!int.TryParse(sizeStr, out size))
+    {
+        Console.WriteLine("Вы ввели не число!");
+    }
+    else break;
+}
+size = int.Parse(sizeStr);
+int[] array = Array(size);
+
+
+int[] Array(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size;)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите число:");
+            inputStr = Console.ReadLine();
+            if (!int.TryParse(inputStr, out input))
+            {
+                Console.WriteLine("Вы ввели не число!");
+            }
+            else
+            {
+                input = int.Parse(inputStr);
+                arr[i] = input;
+                i++;
+                break;
+            }
+        }
+    }
+    return arr;
+}
+
+more = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i] > 0) more++;
+}
+
+Console.WriteLine($"В заполненом массиве пользавателем [{String.Join(", ", array)}] чисел больше нуля равно = {more}");
---
+++ Home_Work_43/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.Clear();
+
+double b1, k1, b2, k2, pointY1, decisionX, decision, pointY2;
+
+Console.WriteLine("Для нахождения точки пересечения двух прямых будем использовать уравнение y = k1 * x + b1, y = k2 * x + b2. Значения b1, k1, b2 и k2 задаются вами.");
+Console.WriteLine("Введите k1: ");
+k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b1: ");
+b1 = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Введите k2: ");
+k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+b2 = double.Parse(Console.ReadLine());
+
+// для нахождения первой точки мы вычитаем из первого уравнения второе
+// y = k1 * x + b1                     y - y = k1x + b1 - (k2x + b2)
+// y = k2 * x + b2                     y = k2 * x + b2 
+pointY1 = 0;
+decisionX = k1 - k2; // находим разницу между k1x и k2x
+decision = b1 - b2; // находим разницу между b1 и b2
+pointY1 = (decision / decisionX) * -1; // делим число без Х на число с Х (х (икс оставляем слева) = (decision / decisionX)). * -1 нужен для смены знака.
+
+
+// для нахождения второй точки нужно подставить значение pointY1 во второе уравнение
+// х = pointY1
+// y = k2 * (x) + b2
+pointY2 = 0;
+pointY2 = k2 * pointY1 + b2;
+
+
+Console.WriteLine($"Точки пересечения двух прямых будет {pointY1:f1} и {pointY2:f1}");
